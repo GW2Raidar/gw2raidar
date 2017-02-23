d1454032d43521fd98809c99237c43ed454e7132
Goran Topic <amadanmath+github@gmail.com>
2017-02-23 18:14:49 +0900

    First attempt at NumPy/Pandas-backed parser

--- evtcparser/parser.py
+++ evtcparser/parser.py
@@ -1,17 +1,19 @@
-import struct
 import re
-from enum import Enum
+from enum import IntEnum
+import struct
+import numpy as np
+import pandas as pd
 
 ENCODING = "utf8"
 
-class Activation(Enum):
+class Activation(IntEnum):
     NONE = 0
     NORMAL = 1
     QUICKNESS = 2
     CANCEL_FIRE = 3
     CANCEL_CANCEL = 4
 
-class StateChange(Enum):
+class StateChange(IntEnum):
     NORMAL = 0
     ENTER_COMBAT = 1
     EXIT_COMBAT = 2
@@ -25,7 +27,7 @@ class StateChange(Enum):
     LOG_END = 10
 
 #Change to another data type - it's not really an enum?
-class AgentType(Enum):
+class AgentType(IntEnum):
     NO_ID = -1
     UNKNOWN = 0
     GUARDIAN = 1
@@ -43,17 +45,17 @@ class AgentType(Enum):
         return AgentType.GUARDIAN.value <= self.value <= AgentType.REVENANT.value
 
 
-class CustomSkill(Enum):
+class CustomSkill(IntEnum):
     RESURRECT = 1066
     BANDAGE = 1175
     DODGE = 65001
 
-class IFF(Enum):
+class IFF(IntEnum):
     FRIEND = 0
     FOE = 1
     UNKNOWN = 2
 
-class Result(Enum):
+class Result(IntEnum):
     NORMAL=0
     CRIT=1
     GLANCE=2
@@ -64,7 +66,7 @@ class Result(Enum):
     BLIND=7
     KILLING_BLOW=8
 
-class Boon(Enum):
+class Boon(IntEnum):
     MIGHT = 1
     QUICKNESS = 2
     FURY = 3
@@ -119,36 +121,86 @@ class Skill:
         self.id, name = struct.unpack("<l64s", data)
         self.name = name.decode(ENCODING).rstrip('\0')
 
-class Event:
-    def __init__(self, data):
-        self.time, self.src_agent, self.dst_agent, self.value, self.buff_dmg, self.overstack_value, self.skill_id, self.src_instid, self.dst_instid, self.src_master_instid, self.iss_offset, self.iss_offset_target, self.iss_bd_offset, self.iss_bd_offset_target, self.iss_alt_offset, self.iss_alt_offset_target, self.skar, self.skar_alt, self.skar_use_alt, self.iff, self.buff, result, self.is_activation, self.is_buffremove, self.is_ninety, self.is_fifty, self.is_moving, state_change, self.is_flanking, self.result_local, self.ident_local = struct.unpack("<QQQllHHHHHBBBBBBBBBBBBBBBBBBBBBx", data)
-        self.state_change = StateChange(state_change)
-        self.result = Result(result)
+AGENT_DTYPE = np.dtype([
+        ('addr', np.uint64),
+        ('prof', np.int32),
+        ('elite', np.int32),
+        ('toughness', np.int32),
+        ('healing', np.int32),
+        ('condition', np.int32),
+        ('name', '|S64'),
+    ], True)
+
+SKILL_DTYPE = np.dtype([
+        ('id', np.int32),
+        ('name', 'S64'),
+    ], True)
+
+EVENT_DTYPE = np.dtype([
+        ('time', np.uint64),
+        ('src_agent', np.uint64),
+        ('dst_agent', np.uint64),
+        ('value', np.int32),
+        ('buff_dmg', np.int32),
+        ('overstack_value', np.uint16),
+        ('skillid', np.uint16),
+        ('src_instid', np.uint16),
+        ('dst_instid', np.uint16),
+        ('src_master_instid', np.uint16),
+        ('iss_offset', np.uint8),
+        ('iss_offset_target', np.uint8),
+        ('iss_bd_offset', np.uint8),
+        ('iss_bd_offset_target', np.uint8),
+        ('iss_alt_offset', np.uint8),
+        ('iss_alt_offset_target', np.uint8),
+        ('skar', np.uint8),
+        ('skar_aly', np.uint8),
+        ('skar_use_alt', np.uint8),
+        ('iff', np.uint8),
+        ('buff', np.uint8),
+        ('result', np.uint8),
+        ('is_activation', np.uint8),
+        ('is_buffremove', np.uint8),
+        ('is_ninety', np.uint8),
+        ('is_fifty', np.uint8),
+        ('is_moving', np.uint8),
+        ('state_change', np.uint8),
+        ('is_flanking', np.uint8),
+        ('result_local', np.uint8),
+        ('ident_local', np.uint8),
+    ], True)
 
 class Encounter:
     def __init__(self, file):
-        evtc, self.version, self.area_id, _ = struct.unpack("<4s9sHc", file.read(16))
+        evtc, version, self.area_id = struct.unpack("<4s9sHx", file.read(16))
         if evtc != b"EVTC":
             raise FileFormatException("Not an EVTC file")
+        self.version = version.decode(ENCODING).rstrip('\0')
 
         num_agents, = struct.unpack("<i", file.read(4))
-        self.agents = [Agent(file.read(96)) for _ in range(num_agents)]
+        self.agents = pd.DataFrame(np.fromfile(file, dtype=AGENT_DTYPE, count=num_agents))
+        split = self.agents.name.str.split(b'\x00', expand=True)
+        self.agents['name'] = split[0].str.decode(ENCODING)
+        self.agents['account'] = split[1].str.decode(ENCODING)
+        self.agents['party'] = split[2].fillna(0).astype(np.uint8)
 
         num_skills, = struct.unpack("<i", file.read(4))
-        self.skills = [Skill(file.read(68)) for _ in range(num_skills)]
+        self.skills = pd.DataFrame(np.fromfile(file, dtype=SKILL_DTYPE, count=num_skills))
+        self.skills['name'] = self.skills['name'].str.decode(ENCODING)
 
-        self.events = []
-        while True:
-            data = file.read(64)
-            if not data: break
-            self.events.append(Event(data))
+        self.events = pd.DataFrame(np.fromfile(file, dtype=EVENT_DTYPE))
 
-        agent_addrs = dict((agent.addr, agent) for agent in self.agents)
-        for event in self.events:
-            if event.src_agent in agent_addrs:
-                agent_addrs[event.src_agent].set_inst_id(event.src_instid)
-            if event.dst_agent in agent_addrs:
-                agent_addrs[event.dst_agent].set_inst_id(event.dst_instid)
+        self.started_at = self.events[self.events.state_change == StateChange.LOG_START]['value'].iloc[0]
+        self.ended_at = self.events[self.events.state_change == StateChange.LOG_END]['value'].iloc[-1]
+
+        src_agent_map = self.events[['src_agent', 'src_instid']].rename(columns={ 'src_agent': 'addr', 'src_instid': 'inst_id'})
+        dst_agent_map = self.events[['dst_agent', 'dst_instid']].rename(columns={ 'dst_agent': 'addr', 'dst_instid': 'inst_id'})
+        agent_map = pd.concat([src_agent_map, dst_agent_map]).drop_duplicates().set_index('addr')
+        self.agents = self.agents.set_index('addr').join(agent_map)
 
 
+def main():
+    with open('/Users/amadan/Downloads/20170222/20170222-204228.evtc', 'rb') as file:
+        return Encounter(file)
 
+main()
