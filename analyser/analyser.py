0a1ba5ce8bd26f0cb05d5b3203ce4b4d2632d8cb
Goran Topic <amadanmath+github@gmail.com>
2017-02-28 13:21:52 +0900

    Better event classification

--- analyser/analyser.py
+++ analyser/analyser.py
@@ -88,21 +88,28 @@ EVENT_TYPES = {
         (False, False, False): 'state_change', #'weird_unbuff',
     }
 
-class Analyser:
-    def _categorise_agents(self, agents):
-        agents['archetype'] = 1 # POWER
-        agents.loc[agents.condition >= 7, 'archetype'] = 2  # CONDI
-        agents.loc[agents.toughness >= 7, 'archetype'] = 3  # TANK
-        agents.loc[agents.healing >= 7, 'archetype'] = 4    # HEAL
+BOSS_IDS = {
+        0x3C4E: [0x3C4E],                   # Vale Guardian
+        0x3C45: [0x3C45],                   # Gorseval
+        0x3C0F: [0x3C0F],                   # Sabetha
+        0x3EFB: [0x3EFB],                   # Slothasor
+        0x3ED8: [0x3ED8, 0x3F09, 0x3EFD],   # Berg, Zane, Narella
+        0x3EF3: [0x3EF3],                   # Matthias
+        0x3F6B: [0x3F6B],                   # Keep Construct
+        0x3F76: [0x3F76, 0x3F9E],           # Xera
+        0x432A: [0x432A],                   # Cairn
+        0x4314: [0x4314],                   # Mursaat Overseer
+        0x4324: [0x4324],                   # Samarog
+        0x4302: [0x4302],                   # Deimos
+    }
 
+class Analyser:
     def __init__(self, encounter):
         self.encounter = encounter
-        #self.time = encounter.ended_at - encounter.started_at
 
         # ultimate source (e.g. if necro minion attacks, the necro himself)
         events = encounter.events
-
-        self._categorise_agents(agents)
+        agents = encounter.agents
 
         events['ult_src_instid'] = events.src_master_instid.where(events.src_master_instid != 0, events.src_instid)
 
@@ -114,20 +121,53 @@ class Analyser:
         last_aware_as_dst = aware_as_dst.last()
         first_aware = pd.DataFrame([first_aware_as_src, first_aware_as_dst]).min().astype(np.uint64)
         last_aware = pd.DataFrame([last_aware_as_src, last_aware_as_dst]).max().astype(np.uint64)
-        self.agents = encounter.agents.assign(first_aware=first_aware, last_aware=last_aware)
-        self.encounter_start = first_aware.min()
-        self.encounter_end = last_aware.max()
+        agents = agents.assign(first_aware=first_aware, last_aware=last_aware)
 
-        self.players = self.agents[self.agents.party != 0]
-        player_events = events.join(self.players, how='right', on='ult_src_instid')
+        bosses = agents[agents.prof.isin(BOSS_IDS[encounter.area_id])]
+        encounter_start = bosses.first_aware.min()
+        encounter_end = bosses.last_aware.max()
 
-        grouped_events = player_events.groupby([events.buff != 0, events.state_change == parser.StateChange.NORMAL, events.buff_dmg > 0])
-        condi_damage_by_player = grouped_events.get_group((True, True, True)).groupby('ult_src_instid')
-        direct_damage_by_player = grouped_events.get_group((False, True, False)).groupby('ult_src_instid')
-        self.damage = self.players.assign(
-                condi = condi_damage_by_player['buff_dmg'].sum(),
-                direct = direct_damage_by_player['value'].sum(),
+        # archetypes
+        agents['archetype'] = 0
+        agents.loc[agents.party != 0, 'archetype'] = 1     # POWER
+        agents.loc[agents.condition >= 7, 'archetype'] = 2  # CONDI
+        agents.loc[agents.toughness >= 7, 'archetype'] = 3  # TANK
+        agents.loc[agents.healing >= 7, 'archetype'] = 4    # HEAL
+
+        # get player events
+        players = agents[agents.party != 0]
+        player_events = events.join(players[['name', 'account']], how='right', on='ult_src_instid')
+        player_events = player_events[player_events.time.between(encounter_start, encounter_end)]
+
+        not_state_change_events = player_events[player_events.state_change == parser.StateChange.NORMAL]
+
+        # on cbtitem.is_activation == cancel_fire or cancel_cancel, value will be the ms duration of the approximate channel.
+        cancel_fire_events = not_state_change_events[not_state_change_events.is_activation == parser.Activation.CANCEL_FIRE]
+        cancel_cancel_events = not_state_change_events[not_state_change_events.is_activation == parser.Activation.CANCEL_CANCEL]
+        not_cancel_events = not_state_change_events[not_state_change_events.is_activation < parser.Activation.CANCEL_FIRE]
+
+        # on cbtitem.is_buffremove, value will be the duration removed (negative) equal to the sum of all stacks.
+        statusremove_events = not_cancel_events[not_cancel_events.is_buffremove != 0]
+        not_statusremove_events = not_cancel_events[not_cancel_events.is_buffremove == 0]
+
+        # if they are all 0, it will be a buff application (!cbtitem.is_buffremove && cbtitem.is_buff) or physical hit (!cbtitem.is_buff).
+        status_events = not_statusremove_events[not_statusremove_events.buff != 0]
+
+        # on physical, cbtitem.value will be the damage done (positive).
+        # on physical, cbtitem.result will be the result of the attack.
+        hit_events = not_statusremove_events[not_statusremove_events.buff == 0]
+
+        # on buff && !cbtitem.buff_dmg, cbtitem.value will be the millisecond duration.
+        # on buff && !cbtitem.buff_dmg, cbtitem.overstack_value will be the current smallest stack duration in ms if over the buff's stack cap.
+        apply_events = status_events[status_events.value != 0]
+
+        # on buff && !cbtitem.value, cbtitem.buff_dmg will be the approximate damage done by the buff.
+        condi_events = status_events[status_events.value == 0]
+
+        condi_damage_by_player = condi_events.groupby('ult_src_instid')['buff_dmg'].sum()
+        direct_damage_by_player = hit_events.groupby('ult_src_instid')['value'].sum()
+
+        self.damage = players.assign(
+                condi = condi_damage_by_player,
+                direct = direct_damage_by_player,
             )
-
-        self.key_target_ids = {encounter.area_id}
-
