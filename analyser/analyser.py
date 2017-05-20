398f1c0aaa0bdfcf0d1fcdd9019957c7355e8ea8
Toeofdoom <matthewelveyprice@gmail.com>
2017-05-20 20:19:04 +1000

    Initial version of incoming damage parsing complete

--- analyser/analyser.py
+++ analyser/analyser.py
@@ -13,8 +13,10 @@ import timeit
 class Group:
     CATEGORY = "Category"
     PLAYER = "Player"
+    BOSS = "Boss"
     PHASE = "Phase"
     DESTINATION = "To"
+    SOURCE = "From"
     SKILL = "Skill"
     SUBGROUP = "Subgroup"
     BUFF = "Buff"
@@ -110,6 +112,14 @@ def unique_names(dictionary):
 def create_mapping(df, column):
     return unique_names(df.to_dict()[column])
 
+def filter_damage_events(events):
+    damage_events = events[(events.type == LogType.POWER) |(events.type == LogType.CONDI)]
+    damage_events = damage_events.assign(damage =
+                                         np.where(damage_events.type == LogType.POWER,
+                                                  damage_events['value'],
+                                                  damage_events['buff_dmg']))
+    return damage_events[damage_events.damage > 0]
+
 class Analyser:
     def preprocess_agents(self, agents, collector):
         players = agents[agents.party != 0]
@@ -129,7 +139,8 @@ class Analyser:
         events['ult_src_instid'] = events.src_master_instid.where(
             events.src_master_instid != 0, events.src_instid)
         events = assign_event_types(events)
-        player_events = events[events.ult_src_instid.isin(self.player_instids)].sort_values(by='time')
+        player_src_events = events[events.ult_src_instid.isin(self.player_instids)].sort_values(by='time')
+        player_dst_events = events[events.dst_instid.isin(self.player_instids)].sort_values(by='time')
         boss_events = events[events.dst_instid.isin(self.boss_instids)]
         final_boss_events = boss_events[boss_events.dst_instid.isin(self.boss_instids)]
         boss_power_events = boss_events[(boss_events.type == LogType.POWER) & (boss_events.value > 0)]
@@ -142,14 +153,20 @@ class Analyser:
         print("Autodetected phases: {0} {1}".format(phase_starts, phase_ends))
         self.phases = list(zip(phase_starts, phase_ends))
 
-        return player_events, boss_events, final_boss_events
+        return player_src_events, player_dst_events, boss_events, final_boss_events
 
     def preprocess_skills(self, skills, collector):
         collector.set_context_value(ContextType.SKILL_NAME, create_mapping(skills, 'name'))
 
     def __init__(self, encounter):
         self.boss_info = BOSSES[encounter.area_id]
-        collector = Collector.root([Group.CATEGORY, Group.PHASE, Group.PLAYER, Group.DESTINATION, Group.SKILL, Group.BUFF])
+        collector = Collector.root([Group.CATEGORY,
+                                    Group.PHASE,
+                                    Group.PLAYER,
+                                    Group.SOURCE,
+                                    Group.DESTINATION,
+                                    Group.SKILL,
+                                    Group.BUFF])
 
         #set up data structures
         events = encounter.events
@@ -157,7 +174,7 @@ class Analyser:
         skills = encounter.skills
         players, bosses, final_bosses = self.preprocess_agents(agents, collector)
         self.preprocess_skills(skills, collector)
-        player_events, boss_events, final_boss_events = self.preprocess_events(events)
+        player_src_events, player_dst_events, boss_events, final_boss_events = self.preprocess_events(events)
 
         #time constraints
         start_event = events[events.state_change == parser.StateChange.LOG_START]
@@ -165,13 +182,13 @@ class Analyser:
         start_time = start_event['time'][0]
         encounter_end = events.time.max()
 
-        buff_data = BuffPreprocessor().process_events(start_time, encounter_end, skills, players, player_events)
+        buff_data = BuffPreprocessor().process_events(start_time, encounter_end, skills, players, player_src_events)
 
-        collector.with_key(Group.CATEGORY, "boss").run(self.collect_boss_status, bosses)
         collector.with_key(Group.CATEGORY, "boss").run(self.collect_boss_key_events, events)
         collector.with_key(Group.CATEGORY, "status").run(self.collect_player_status, players)
-        collector.with_key(Group.CATEGORY, "status").run(self.collect_player_key_events, player_events)
-        collector.with_key(Group.CATEGORY, "damage").run(self.collect_damage, player_events)
+        collector.with_key(Group.CATEGORY, "status").run(self.collect_player_key_events, player_src_events)
+        collector.with_key(Group.CATEGORY, "damage").run(self.collect_outgoing_damage, player_src_events)
+        collector.with_key(Group.CATEGORY, "damage").run(self.collect_incoming_damage, player_dst_events)
         collector.with_key(Group.CATEGORY, "buffs").run(self.collect_buffs_by_target, buff_data)
 
         encounter_collector = collector.with_key(Group.CATEGORY, "encounter")
@@ -188,12 +205,6 @@ class Analyser:
 
     # section: Agent stats (player/boss
     # subsection: boss stats
-    def collect_invididual_boss_status(self, collector, boss):
-        collector.add_data("Exists", True)
-
-    def collect_boss_status(self, collector, bosses):
-        collector.group(self.collect_invididual_boss_status, bosses, ('name', 'Name'))
-
     def collect_invididual_boss_key_events(self, collector, events):
         #all_state_changes = events[events.state_change != parser.StateChange.NORMAL]
         enter_combat_time = only_entry(events[events.state_change == parser.StateChange.ENTER_COMBAT].time)
@@ -204,7 +215,7 @@ class Analyser:
     def collect_boss_key_events(self, collector, events):
         boss_events = events[events.ult_src_instid.isin(self.boss_instids)]
         collector.group(self.collect_invididual_boss_key_events, boss_events,
-                        ('ult_src_instid', 'Player', mapped_to(ContextType.AGENT_NAME)))
+                        ('ult_src_instid', Group.BOSS, mapped_to(ContextType.AGENT_NAME)))
 
     #subsection: player stats
     def collect_player_status(self, collector, players):
@@ -213,7 +224,7 @@ class Analyser:
         players.loc[players.condition >= 7, 'archetype'] = Archetype.CONDI
         players.loc[players.toughness >= 7, 'archetype'] = Archetype.TANK
         players.loc[players.healing >= 7, 'archetype'] = Archetype.HEAL
-        collector.group(self.collect_individual_player_status, players, ('name', 'Player'))
+        collector.group(self.collect_individual_player_status, players, ('name', Group.PLAYER))
 
     def collect_individual_player_status(self, collector, player):
         only_entry = player.iloc[0]
@@ -240,112 +251,138 @@ class Analyser:
         collector.add_data("EnterCombat", enter_combat_time, int)
         collector.add_data("Death", death_time, int)
 
-    #section: Damage stats
-    #subsection: Filtering events
-    def collect_damage(self, collector, player_events):
-        #prepare damage_events
-        damage_events = player_events[(player_events.type == LogType.POWER)
-                                      |(player_events.type == LogType.CONDI)]
-        damage_events = damage_events.assign(
-            damage = np.where(damage_events.type == LogType.POWER,
-                              damage_events['value'], damage_events['buff_dmg']))
-        damage_events = damage_events[damage_events.damage > 0]
+    #Split definitions
+    def split_by_phase(self, collector, method, events):
+        def collect_phase(name, phase_events):
+            duration = float(phase_events['time'].max() - phase_events['time'].min())/1000.0
+            if not duration > 0.001:
+                duration = 0
+            collector.set_context_value(ContextType.DURATION, duration)
+            collector.with_key(Group.PHASE, name).run(method, phase_events)
 
-        #determine phases
-        collector.with_key(Group.PHASE, "All").run(self.collect_phase_damage, damage_events)
+        collect_phase("All", events)
+        collect_phase("None", events[0:0])
         for i in range(0,len(self.phases)):
             phase = self.phases[i]
-            phase_events = damage_events[(damage_events.time >= phase[0]) & (damage_events.time <= phase[1])]
-            collector.with_key(Group.PHASE, "{0}".format(i+1)).run(self.collect_phase_damage, phase_events)
+            phase_events = events[(events.time >= phase[0]) & (events.time <= phase[1])]
+            collect_phase("{0}".format(i+1), phase_events)
+
+    def split_by_player_groups(self, collector, method, events, player_column):
+        collector.run(method, events)
+        collector.with_key(Group.SUBGROUP, "*None").run(method, events[0:0])
+        for subgroup in self.subgroups:
+            subgroup_players = self.subgroups[subgroup]
+            subgroup_events = events[player_column.isin(subgroup_players)]
+            collector.with_key(Group.SUBGROUP, "{0}".format(subgroup)).run(
+                method, subgroup_events)
+
+        collector.group(method, events,
+                        ('ult_src_instid', Group.PLAYER, mapped_to(ContextType.AGENT_NAME)))
+
+    def split_by_agent(self, collector, method, events, group, enemy_column):
+        boss_events = events[events[enemy_column].isin(self.boss_instids)]
+        player_events = events[events[enemy_column].isin(self.player_instids)]
+
+        non_add_instids = self.boss_instids
+        add_events = events[events[enemy_column].isin(non_add_instids) != True]
+
+        collector.with_key(group, "*All").run(method, events)
+        collector.with_key(group, "*Boss").run(method, boss_events)
+        collector.with_key(group, "*Players").run(method, player_events)
+        collector.with_key(group, "*Adds").run(method, add_events)
+        collector.with_key(group, "*None").run(method, events[0:0])
+        if len(self.boss_instids) > 1:
+             collector.group(method, boss_events,
+                (enemy_column, group, mapped_to(ContextType.AGENT_NAME)))
+
+    #section: Damage stats
+    #subsection: Filtering events
+    def collect_incoming_damage(self, collector, player_events):
+        #prepare damage_events
+        damage_events = filter_damage_events(player_events)
+
+        #determine phases
+        self.split_by_phase(collector, self.collect_phase_incoming_damage, damage_events)
+
+    def collect_outgoing_damage(self, collector, player_events):
+        #prepare damage_events
+        damage_events = filter_damage_events(player_events)
+
+        #determine phases
+        self.split_by_phase(collector, self.collect_phase_damage, damage_events)
 
     def collect_phase_damage(self, collector, damage_events):
-        if damage_events.empty:
-            raise EvtcAnalysisException('No damage events detected: Phase %s' % collector.context[Group.PHASE])
+        collector.with_key(Group.DESTINATION, "*All").run(self.collect_skill_data, damage_events)
+        self.split_by_agent(collector,
+                            self.collect_destination_damage,
+                            damage_events,
+                            Group.DESTINATION,
+                            'dst_instid')
 
-        collector.set_context_value(
-            ContextType.DURATION,
-            float(damage_events['time'].max() - damage_events['time'].min())/1000.0)
-
-        boss_events = damage_events[damage_events.dst_instid.isin(self.boss_instids)]
-        add_events = damage_events[damage_events.dst_instid.isin(self.boss_instids) != True]
-
-        collector.with_key(Group.DESTINATION, "*All").run(self.collect_destination_damage_with_skill_data, damage_events)
-        collector.with_key(Group.DESTINATION, "*Boss").run(self.collect_destination_damage, boss_events)
-        collector.with_key(Group.DESTINATION, "*Adds").run(self.collect_destination_damage, add_events)
-        if len(self.boss_instids) > 1:
-             collector.group(self.collect_destination_damage, boss_events,
-                ('dst_instid', Group.DESTINATION, mapped_to(ContextType.AGENT_NAME)))
+    def collect_phase_incoming_damage(self, collector, damage_events):
+        #self.split_by_agent(collector,
+        #                    self.collect_destination_damage,
+        #                    damage_events,
+        #                    Group.SOURCE,
+        #                    'ult_src_instid')
+        collector.set_context_value(ContextType.TOTAL_DAMAGE_FROM_SOURCE_TO_DESTINATION,
+                                    damage_events['damage'].sum())
+        collector.with_key(Group.SOURCE, "*All").group(
+            self.aggregate_basic_damage_stats, damage_events,
+            ('dst_instid', Group.PLAYER, mapped_to(ContextType.AGENT_NAME)))
+        collector.with_key(Group.SOURCE, "*All").group(
+            self.collect_player_incoming_damage, damage_events,
+            ('dst_instid', Group.PLAYER, mapped_to(ContextType.AGENT_NAME)))
 
     def collect_destination_damage(self, collector, damage_events):
         collector.set_context_value(ContextType.TOTAL_DAMAGE_TO_DESTINATION,
                                     damage_events['damage'].sum())
-        collector.run(self.collect_group_damage, damage_events)
-        for subgroup in self.subgroups:
-            subgroup_players = self.subgroups[subgroup]
-            subgroup_events = damage_events[damage_events.ult_src_instid.isin(subgroup_players)]
-            collector.with_key(Group.SUBGROUP, "{0}".format(subgroup)).run(
-                self.collect_group_damage, subgroup_events)
+        self.split_by_player_groups(collector,
+                                    self.aggregate_overall_damage_stats,
+                                    damage_events,
+                                    damage_events.ult_src_instid)
 
-        collector.group(self.collect_individual_damage, damage_events,
-                        ('ult_src_instid', Group.PLAYER, mapped_to(ContextType.AGENT_NAME)))
-
-    def collect_destination_damage_with_skill_data(self, collector, damage_events):
-        self.collect_destination_damage(collector, damage_events)
+    def collect_skill_data(self, collector, damage_events):
         collector.group(self.collect_player_skill_damage, damage_events,
                         ('ult_src_instid', Group.PLAYER, mapped_to(ContextType.AGENT_NAME)))
 
-    #subsection: Aggregating damage
-    def collect_group_damage(self, collector, events):
-        power_events = events[events.type == LogType.POWER]
-        condi_events = events[events.type == LogType.CONDI]
-        # print(events.columns)
-        collector.add_data('scholar', power_events['is_ninety'].mean() if not power_events['is_ninety'].empty else 0, percentage)
-        collector.add_data('seaweed', power_events['is_moving'].mean() if not power_events['is_moving'].empty else 0, percentage)
-        collector.add_data('power', power_events['damage'].sum(), int)
-        collector.add_data('condi', condi_events['damage'].sum(), int)
-        collector.add_data('total', events['damage'].sum(), int)
-        collector.add_data('dps', events['damage'].sum(), per_second(int))
-        collector.add_data('power_dps', power_events['damage'].sum(), per_second(int))
-        collector.add_data('condi_dps', condi_events['damage'].sum(), per_second(int))
-
-    def collect_individual_damage(self, collector, events):
-        power_events = events[events.type == LogType.POWER]
-        condi_events = events[events.type == LogType.CONDI]
-
-        # print(events.columns)
-        collector.add_data('fifty', power_events['is_fifty'].mean(), percentage)
-        collector.add_data('scholar', power_events['is_ninety'].mean(), percentage)
-        collector.add_data('seaweed', power_events['is_moving'].mean(), percentage)
-        collector.add_data('power', power_events['damage'].sum(), int)
-        collector.add_data('condi', condi_events['damage'].sum(), int)
-        collector.add_data('total', events['damage'].sum(), int)
-        collector.add_data('power_dps', power_events['damage'].sum(), per_second(int))
-        collector.add_data('condi_dps', condi_events['damage'].sum(), per_second(int))
-        collector.add_data('dps', events['damage'].sum(), per_second(int))
-        collector.add_data('percentage', events['damage'].sum(),
-                           percentage_of(ContextType.TOTAL_DAMAGE_TO_DESTINATION))
-
-    # subsection: detailed skill data
     def collect_player_skill_damage(self, collector, events):
         power_events = events[events.type == LogType.POWER]
-        condi_events = events[events.type == LogType.CONDI]
         collector.set_context_value(ContextType.TOTAL_DAMAGE_FROM_SOURCE_TO_DESTINATION,
                                     events['damage'].sum())
-        collector.group(self.collect_power_skill_data, power_events,
+        collector.group(self.aggregate_power_damage_stats, power_events,
                         ('skillid', Group.SKILL, mapped_to(ContextType.SKILL_NAME)))
-        collector.group(self.collect_condi_skill_data, condi_events,
+        collector.group(self.aggregate_basic_damage_stats, events,
                         ('skillid', Group.SKILL, mapped_to(ContextType.SKILL_NAME)))
 
-    def collect_power_skill_data(self, collector, events):
+    def collect_player_incoming_damage(self, collector, events):
+        collector.set_context_value(ContextType.TOTAL_DAMAGE_FROM_SOURCE_TO_DESTINATION,
+                                    events['damage'].sum())
+        collector.group(self.aggregate_basic_damage_stats, events,
+                        ('skillid', Group.SKILL, mapped_to(ContextType.SKILL_NAME)))
+
+    #subsection: Aggregating damage
+    def aggregate_overall_damage_stats(self, collector, events):
+        power_events = events[events.type == LogType.POWER]
+        condi_events = events[events.type == LogType.CONDI]
+
+        collector.set_context_value(ContextType.TOTAL_DAMAGE_FROM_SOURCE_TO_DESTINATION,
+                                    events['damage'].sum())
+
+        self.aggregate_power_damage_stats(collector, power_events)
+        self.aggregate_basic_damage_stats(collector, events)
+        collector.add_data('power', power_events['damage'].sum(), int)
+        collector.add_data('condi', condi_events['damage'].sum(), int)
+        collector.add_data('power_dps', power_events['damage'].sum(), per_second(int))
+        collector.add_data('condi_dps', condi_events['damage'].sum(), per_second(int))
+
+
+    def aggregate_power_damage_stats(self, collector, events):
         collector.add_data('fifty', events['is_fifty'].mean(), percentage)
         collector.add_data('scholar', events['is_ninety'].mean(), percentage)
         collector.add_data('seaweed', events['is_moving'].mean(), percentage)
-        collector.add_data('total', events['damage'].sum(), int)
-        collector.add_data('dps', events['damage'].sum(), per_second(int))
-        collector.add_data('percentage', events['damage'].sum(),
-                           percentage_of(ContextType.TOTAL_DAMAGE_FROM_SOURCE_TO_DESTINATION))
 
-    def collect_condi_skill_data(self, collector, events):
+    def aggregate_basic_damage_stats(self, collector, events):
         collector.add_data('total', events['damage'].sum(), int)
         collector.add_data('dps', events['damage'].sum(), per_second(int))
         collector.add_data('percentage', events['damage'].sum(),
