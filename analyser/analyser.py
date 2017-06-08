2ac711614d55f94914434171f1ecfedcc8203648
Toeofdoom <matthewelveyprice@gmail.com>
2017-06-09 00:37:25 +1000

    Improved phase detection algorithm - could still break with high dps + high latency for deimos

--- analyser/analyser.py
+++ analyser/analyser.py
@@ -96,8 +96,8 @@ class Phase:
         if self.phase_end_health is not None:
             relevant_health_updates = health_updates[(health_updates.time >= current_time) &
                                                      (health_updates.dst_agent >= self.phase_end_health * 100)]
-
-            if relevant_health_updates.empty or relevant_health_updates['dst_agent'].min() > (self.phase_end_health + 1) * 100:
+            print_frame(relevant_health_updates)
+            if relevant_health_updates.empty or health_updates['dst_agent'].min() > (self.phase_end_health + 2) * 100:
                 return None
             end_time = current_time = int(relevant_health_updates['time'].iloc[-1])
             print("{0}: Detected health below {1} at time {2}".format(self.name, self.phase_end_health, current_time))
