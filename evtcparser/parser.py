3132f2134ede12a79369485416d9c5716e6533e4
Immortius <immortius@gmail.com>
2017-07-23 23:21:26 +1000

    #107 Fixed a bug in which I hadn't set the index for agents correctly Improved the inst_id mapping (turns out I didn't want group by after all)

--- evtcparser/parser.py
+++ evtcparser/parser.py
@@ -176,6 +176,14 @@ class Encounter:
         self.log_started_at = self.events[self.events.state_change == StateChange.LOG_START]['value'].iloc[0]
         self.log_ended_at = self.events[self.events.state_change == StateChange.LOG_END]['value'].iloc[-1]
 
+    def _old_add_inst_id_to_agents(self):
+        self.raw_agents = self.agents
+        src_agent_map = self.events[['src_agent', 'src_instid']].rename(columns={ 'src_agent': 'addr', 'src_instid': 'inst_id'})
+        dst_agent_map = self.events[['dst_agent', 'dst_instid']].rename(columns={ 'dst_agent': 'addr', 'dst_instid': 'inst_id'})
+        agent_map = pd.concat([src_agent_map, dst_agent_map])
+        agent_map = agent_map[agent_map.inst_id != 0].drop_duplicates().set_index('addr')
+        self.agents = self.agents.set_index('addr').join(agent_map).groupby('inst_id').first()
+        
     def _add_inst_id_to_agents(self):
         src_agent_map = self.events[['time', 'src_agent', 'src_instid']].rename(columns={ 'src_agent': 'addr', 'src_instid': 'inst_id'})
         dst_agent_map = self.events[['time', 'dst_agent', 'dst_instid']].rename(columns={ 'dst_agent': 'addr', 'dst_instid': 'inst_id'})
@@ -183,17 +191,14 @@ class Encounter:
         agent_map = pd.concat([src_agent_map, dst_agent_map])
         agent_map = agent_map[agent_map.inst_id != 0]
         agent_map = agent_map.sort_values(by='time')
-        agent_map = agent_map.groupby(['addr']).first()
-        agent_map = agent_map.sort_values(by='time')
+        agent_map.drop_duplicates(subset='addr',inplace=True)
         agent_map.insert(0, 'new_id', range(1, 1 + len(agent_map)))
-        agent_map = agent_map.reset_index()
         
         del self.events['src_instid']
         del self.events['dst_instid']
         self.events = pd.merge(left=self.events,right=agent_map[['addr', 'new_id']].rename(columns={'addr' : 'src_agent', 'new_id' : 'src_instid'}), how='left', left_on='src_agent', right_on='src_agent')
         self.events = pd.merge(left=self.events,right=agent_map[['addr', 'new_id']].rename(columns={'addr' : 'dst_agent', 'new_id' : 'dst_instid'}), how='left')
-        instid_map = agent_map.groupby(['inst_id']).first()
-        instid_map = instid_map.reset_index()
+        instid_map = agent_map.drop_duplicates(subset='inst_id',inplace=False)
         self.events = self.events.rename(columns={'src_master_instid' : 'old_src_master_instid'})
         self.events = pd.merge(left=self.events,right=instid_map[['inst_id', 'new_id']].rename(columns={'inst_id' : 'old_src_master_instid', 'new_id' : 'src_master_instid'}), how='left') 
         self.events.fillna(0, inplace=True)
@@ -202,6 +207,7 @@ class Encounter:
         #self.addr_agents = self.agents.set_index('addr')
         # deal with duplicate inst_id for different addrs
         self.agents = pd.merge(left=self.agents,right=agent_map[['addr', 'new_id']].rename(columns={'new_id' : 'inst_id'}), how='left')
+        self.agents = self.agents.set_index('inst_id')
         del self.events['old_src_master_instid']
         del self.agents['addr']
 
