f2eceec7be67bb0d97b4b550d65890eb58f2c628
Goran Topic <amadanmath+github@gmail.com>
2017-07-28 14:46:58 +0900

    Make various things in admin readonly

--- raidar/admin.py
+++ raidar/admin.py
@@ -6,20 +6,27 @@ class ParticipationInline(admin.TabularInline):
     model = Participation
     extra = 10
     max_num = 10
+    readonly_fields = ('character', 'elite', 'party')
 
 class EncounterAdmin(admin.ModelAdmin):
     inlines = (ParticipationInline,)
+    readonly_fields = ('url_id', 'started_at', 'duration', 'uploaded_at', 'uploaded_by', 'area', 'filename')
 
     # hack, but... ugly otherwise
     class Media:
         css = { 'all' : ('raidar/hide_admin_original.css',) }
 
+class CharacterAdmin(admin.ModelAdmin):
+    readonly_fields = ('account', 'name', 'profession')
+
 class CharacterInline(admin.TabularInline):
     model = Character
+    readonly_fields = ('name', 'profession')
     extra = 1
 
 class AccountAdmin(admin.ModelAdmin):
     inlines = (CharacterInline,)
+    readonly_fields = ('name',)
 
     # hack, but... ugly otherwise
     class Media:
@@ -28,6 +35,6 @@ class AccountAdmin(admin.ModelAdmin):
 admin.site.register(Area)
 admin.site.register(Era)
 admin.site.register(Account, AccountAdmin)
-admin.site.register(Character)
+admin.site.register(Character, CharacterAdmin)
 admin.site.register(Encounter, EncounterAdmin)
 admin.site.register(UserProfile)
