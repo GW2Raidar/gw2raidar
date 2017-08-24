dc693a502ba10a4f54a4a4ff98b2d312c2438956
Goran Topic <amadanmath+github@gmail.com>
2017-08-24 11:38:37 +0900

    Issue #32 (user profile) further changes

--- raidar/management/commands/restat.py
+++ raidar/management/commands/restat.py
@@ -189,6 +189,7 @@ class Command(BaseCommand):
                             player_archetype_encounter = categorise(True, True, False)
                             player_build_encounter = categorise(True, True, True)
                             player_profession_encounter = categorise(True, False, True)
+                            player_all = categorise(False, False, False)
 
                             stats_in_phase_to_all = player_stats['Metrics']['damage']['To']['*All']
                             stats_in_phase_events = player_stats['Metrics']['events']
@@ -203,11 +204,12 @@ class Command(BaseCommand):
                                         player_archetype_encounter,
                                         player_build_encounter,
                                         player_profession_encounter]
-                            all = breakdown + [player_summary, player_this_encounter]
+                            all = breakdown + [player_summary, player_this_encounter, player_all]
                             encounter_stats = [player_this_encounter,
                                                player_archetype_encounter,
                                                player_build_encounter,
-                                               player_profession_encounter]
+                                               player_profession_encounter,
+                                               player_all]
 
                             #TODO: Remove if the one in player_summary is enough!
                             get_and_add(totals_for_player, 'count', 1)
