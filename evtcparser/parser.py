b0c7fc498dc437dd434f56f1ab0b90416f59cc97
Goran Topic <amadanmath+github@gmail.com>
2017-09-05 13:46:10 +0000

    Move the cutoff to April patch day

--- evtcparser/parser.py
+++ evtcparser/parser.py
@@ -215,7 +215,7 @@ class Encounter:
     def __init__(self, file):
         try:
             self._read_header(file)
-            if self.version < "20170808":
+            if self.version < "20170419":
                 raise EvtcParseException('Unsupported EVTC version')
             self._read_agents(file)
             self._read_skills(file)
