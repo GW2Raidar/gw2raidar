c348b2d51e736ce05ec522a0967910b6e8975843
Goran Topic <amadanmath+github@gmail.com>
2017-11-09 12:05:06 +0900

    Version bump

--- gw2raidar/settings.py
+++ gw2raidar/settings.py
@@ -14,8 +14,8 @@ import os
 from dateutil import parser
 
 VERSION = {
-        'id': '1.0.2',
-        'timestamp': 1509367406, # date +%s
+        'id': '1.0.3',
+        'timestamp': 1510196685, # date +%s
         }
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
